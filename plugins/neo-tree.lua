7a78cc4fddf7260ebd7b99e9b13494132fd7cc7a
Zhuo Chen <zhuo.chen@compass.com>
2023-08-10 17:41:13 +0800

    set g/ as fuzzy finder for neo-tree

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -4,7 +4,9 @@ return {
     local mappings = config.window.mappings
     -- the fuzzy finder works very slow and always crash when
     -- the number of files is too large. Thus I disable it.
-    mappings["/"] = ""
+    mappings["/"] = "noop"
+    mappings["g/"] = "fuzzy_finder"
+
     return config
   end,
 }
