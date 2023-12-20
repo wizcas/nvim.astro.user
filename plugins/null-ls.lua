0f9bd175f03a86066bc5b683d640a2860f2c0591
Zhuo Chen <zhuo.chen@compass.com>
2023-12-20 15:57:22 +0800

    replace eslint_d with eslint

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -13,10 +13,10 @@ return {
       -- null_ls.builtins.formatting.prettier,
       null_ls.builtins.formatting.stylua,
       null_ls.builtins.formatting.prettierd,
-      null_ls.builtins.formatting.eslint_d,
+      null_ls.builtins.formatting.eslint,
       null_ls.builtins.formatting.stylelint,
-      null_ls.builtins.diagnostics.eslint_d,
-      null_ls.builtins.code_actions.eslint_d,
+      null_ls.builtins.diagnostics.eslint,
+      null_ls.builtins.code_actions.eslint,
     }
     return config -- return final config table
   end,
